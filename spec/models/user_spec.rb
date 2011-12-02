4e00d54e5783015946603db96d6729f6dbd18487
Greg Marshall <greg@onevisionresources.com>
2011-12-01 20:34:02 -0500

    Added password awesomeness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,9 +88,43 @@ describe User do
     it "should have an encrypted password" do
       @user.should respond_to(:encrypted_password)
     end
+    it "should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+    
+    describe "has_password? method" do
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      it "should return true if passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      it "should return false if passwords do not match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+  end
+  
+  describe "authentication method" do
+    it "should exist" do
+      User.should respond_to(:authenticate)
+    end
+    it "should return nil on email/password mismatch" do
+      User.authenticate(@attr[:email], "wrongpass").should be_nil
+    end
+    it "should return nil for an email address with no user" do
+      User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+    end
+    it "should return the user on email/password match" do
+      User.authenticate(@attr[:email], @attr[:password]).should == @user
+    end
   end
   
 end
+
 
 
 # == Schema Information
@@ -103,5 +137,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
