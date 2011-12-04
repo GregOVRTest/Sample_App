9d49952eba7c96cb42cc696235d5860a63d841f0
Greg Marshall <greg@onevisionresources.com>
2011-12-04 16:54:20 -0500

    Finished sign in out function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # Success
+      sign_in user
+      redirect_to user
     end
   end
   
